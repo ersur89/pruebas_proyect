c50a07d6193954b4c5c98de9fdd1c6a15987ed90
Erick <ersur89@gmail.com>
2024-11-05 20:56:25 -0500

    Segunda-api

--- index2.js
+++ index2.js
@@ -1,21 +1,55 @@
-// npm install assemblyai
 
-import { AssemblyAI } from 'assemblyai'
+const {AssemblyAI} = require('assemblyai');
 
 const client = new AssemblyAI({
   apiKey: "a243419e926d4867b0a14a8bfee852b1"
-})
+});
 
-const audioUrl =
-  'https://assembly.ai/sports_injuries.mp3'
+const audioUrl = 'https://github.com/ersur89/videos/raw/main/ejemplo-de-grupo-focal.mp4';
 
 const config = {
-  audio_url: audioUrl
-}
+  audio_url: audioUrl,
+    language_code: 'es',
+    audio_end_at: 280000,
+    audio_start_from: 10
+};
 
 const run = async () => {
-  const transcript = await client.transcripts.transcribe(config)
-  console.log(transcript.text)
-}
+  try {
+    const transcript = await client.transcripts.transcribe(config);
+    console.log(transcript.text);
+    console.log(transcript);
+  } catch (error) {
+    console.error("Error transcribing video:", error);
+  }
+};
 
-run()
+run();
+
+/* const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+
+// Initialize a Gemini model appropriate for your use case.
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const base64Buffer = fs.readFileSync(join(__dirname, ".videos\Ejemplo-grupo-focal.mp3"));
+const base64AudioFile = base64Buffer.toString("base64");
+
+// Generate content using a prompt and the metadata of the uploaded file.
+const result =  model.generateContent([
+    {
+      inlineData: {
+        mimeType: "audio/mp3",
+        data: base64AudioFile
+      }
+    },
+    { text: "Please summarize the audio." },
+  ]);
+
+// Print the response.
+console.log(result.response.text()) */
